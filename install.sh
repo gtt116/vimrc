21ddba0b820a7fe0c3f0a7d849c40f5616a51f53
TianTian Gao <gtt116@126.com>
2012-09-23 17:14:26 +0800

    Add install bash

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ -e ~/.vimrc ]]; then
+    echo 'Backup vimrc to ~/.vimrc.bak'
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+
+if [[ -d ~/.vim ]]; then
+    echo 'Backup vim to ~/.vim.bak'
+    mv ~/.vim ~/.vim.bak
+fi
+
+if [[ -d ~/vimrc ]];then
+    ln -s ~/vimrc/vimrc ~/.vimrc
+    ln -s ~/vimrc/vim ~/.vim
+    echo 'install finished.'
+    exit 0
+else
+    echo '~/vimrc does not exist'
+    exit 0
+fi
