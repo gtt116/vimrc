83af033b5c35f88adc930ce84aa8a00bbb336f37
TianTian Gao <gtt116@126.com>
2012-12-19 16:30:37 +0800

    Ignore *.pyc, and remove vim-gnome

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install exuberant-ctags vim-gnome -y
+sudo apt-get install exuberant-ctags -y
 
 if [[ -e ~/.vimrc ]]; then
     echo 'Backup vimrc to ~/.vimrc.bak'
